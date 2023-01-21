ae9e798d0177ed6f07858ec195addf1bca8b0f86
najeeb <syed.najeeb@gainwelltechnologies.com>
2023-01-21 23:14:49 +0530

    validating mono-repo

---
+++ packaging-demo/src/main.py
@@ -0,0 +1,11 @@
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+def hello_world():
+    return "Hello world, AWS CodeArtifact is very easy."
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
